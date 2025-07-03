8489d4c160ddf8d44a1eaf35827784652d715487
Ninjainpaijama1234 <shauryashikhar1398@gmail.com>
2025-07-03 14:02:18 +0800

    Update app.py

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ st.set_page_config(
 )
 
 # ───────────────────────────── Constants ────────────────────────
-DATA_PATH = Path("UrbanFuelSyntheticSurvey_CLEAN.csv")
+DATA_PATH = Path("UrbanFuelSynthetic3000.csv")
 RND = 42  # random seed
 
 # ─────────────────────── Helper functions ───────────────────────
